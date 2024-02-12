d36c3543061bae08aa5ea774ec74d99992ee5b74
Heeju Kang <hjkang0107@gmail.com>
2024-02-12 19:19:14 +0900

    로그인 프론트, 백 기초

---
+++ app/src/routes/home/home.ctrl.js
@@ -0,0 +1,40 @@
+const output = {
+    home : (req, res) => {
+        res.render("./home/index");
+    },
+    
+    login : (req,res) => {
+        res.render("./home/login")
+    },
+};
+
+const usrs = {
+    id: ["hjkang0107", "hjkang2002", "croma23"],
+    password: ["eric2002@", "eric2002", "ksh106744@"],
+};
+
+const process = {
+    login: (req, res) =>{
+        const id = req.body.id,
+            password = req.body.psword;
+        
+        if(usrs.id.includes(id)) {
+            const idx = usrs.id.indexOf(id);
+            if(usrs.password[idx] == password) {
+                return res.json({
+                    success: true,
+                }); 
+            }
+        }
+
+        return res.json({
+            success: false,
+            msg: "로그인에 실패하였습니다.",
+        });
+    },
+};
+
+module.exports = {
+   output, 
+   process,
+};
